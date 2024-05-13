089fc6e595d9197d236182198f9cbfd1dac7e285
Hamza khan <150890458+Hamja32@users.noreply.github.com>
2024-05-13 17:18:01 +0530

    Create script.js
    search functionality

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+function search(){
+    let filter = document.getElementById('searchBar').value.toUpperCase();
+
+    let item = document.querySelectorAll('.card');
+    
+    let l = document.getElementsByTagName('h3');
+
+    for(var i =0; i<=l.length; i++){
+        let a = item[i].getElementsByTagName('h3')[0];
+
+        let value = a.innerHTML || a.innerText || a.textContent;
+        if(value.toUpperCase().indexOf(filter) >-1){
+            item[i].style.display = "";
+        }else{
+            item[i].style.display = "none";
+        }
+    }
+}
